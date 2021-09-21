19baadb8ccbbf236554375ebf9231e9bf0c416cd
Greg Hamamgian <hamamgian@yahoo.com>
2021-09-21 19:28:02 -0400

    small clean ups/fixes ahead of About page and Donate button

--- fe/tao-of-prog/src/index.js
+++ fe/tao-of-prog/src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import BookContextProvider from './components/BookContext';
 
+require('dotenv').config();
+
+console.log('ENV ENV ENV:', process.env);
+
 ReactDOM.render(
   <BookContextProvider>
     <Router>
